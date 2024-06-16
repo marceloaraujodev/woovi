1626329563844b2b48c1fe30025596d0ec53ddc0
Marcelo Araujo <codetrsh@gmail.com>
2024-06-16 19:23:02 -0300

    deploy button problem test

--- src/components/PixContactForm.jsx
+++ src/components/PixContactForm.jsx
@@ -4,7 +4,7 @@ import Layout from './Layout.jsx';
 import styled from 'styled-components';
 import Title from './Title.jsx';
 import { formattedValue } from '../utils/formatMoney';
-// import Button from './Button.jsx';
+import Button from './Button.jsx';
 import {
   PrazoBox,
   EntradasContainer,
@@ -233,7 +233,7 @@ export default function PixContactForm() {
         </Form>
 
         <div>
-          {/* <Button onClick={handleBtnClick}>Pagar</Button> */}
+          <Button onClick={handleBtnClick}>Pagar</Button>
         </div>
         <PrazoBox>
           Prazo de pagamento:
